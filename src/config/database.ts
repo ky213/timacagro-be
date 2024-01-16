f489dc4faf00eaab7f7f86ad727b1537216bd9ba
YAAQOUB KEDDAOUI <y.keddaoui@gmail.com>
2024-01-16 10:34:27 +0100

    updated pipeline and envs

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm";
 
 export const database = new DataSource({
   type: "postgres",
-  host: process.env.HOST,
-  port: Number(process.env.DB_PORT),
+  host: process.env.DB_HOST,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE_NAME,
+  port: Number(process.env.DB_PORT),
   entities: ["src/services/database/repos/*.repo.ts", "src/services/database/repos/*.repo.js"],
   logging: process.env.NODE_ENV == "prod",
   synchronize: process.env.NODE_ENV == "dev",
